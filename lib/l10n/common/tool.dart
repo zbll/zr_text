6cd1ebbe53b749d7dbff05e769db819164103d3b
{2253697330@qq.com} <2253697330@qq.com>
2023-03-08 15:16:50 +0800

    格式化默认值补充

--- lib/l10n/common/tool.dart
+++ lib/l10n/common/tool.dart
@@ -10,40 +10,41 @@ class Tool {
   }
 
   ///数值格式化120M
-  String number([double? value]) =>
-      value == null ? "" : _appLocalizations.number(value);
+  String number([double? value]) => _appLocalizations.number(value ?? 0);
 
   /// 数字格式化100,000,000
-  String fmtNumber([double? value]) =>
-      value == null ? "" : _appLocalizations.fmtNumber(value);
+  String fmtNumber([double? value]) => _appLocalizations.fmtNumber(value ?? 0);
 
   /// 收缩金额格式化$100M
   String shrinkPrice([double? value]) =>
-      value == null ? "" : _appLocalizations.shrinkPrice(value);
+      _appLocalizations.shrinkPrice(value ?? 0);
 
   /// 展开金额格式化$100,000,000.00
-  String fullPrice([double? value]) =>
-      value == null ? "" : _appLocalizations.fullPrice(value);
+  String fullPrice([double? value]) => _appLocalizations.fullPrice(value ?? 0);
 
   ///年月日国际化
-  String yMd([DateTime? dateTime]) =>
-      dateTime == null ? "" : _appLocalizations.yMd(dateTime);
+  String yMd([DateTime? dateTime]) => dateTime == null
+      ? _appLocalizations.unknown
+      : _appLocalizations.yMd(dateTime);
 
   ///星期国际化
-  String week([DateTime? dateTime]) =>
-      dateTime == null ? "" : _appLocalizations.week(dateTime);
+  String week([DateTime? dateTime]) => dateTime == null
+      ? _appLocalizations.unknown
+      : _appLocalizations.week(dateTime);
 
   ///缩写星期国际化
-  String aabrWeek([DateTime? dateTime]) =>
-      dateTime == null ? "" : _appLocalizations.abbrWeek(dateTime);
+  String aabrWeek([DateTime? dateTime]) => dateTime == null
+      ? _appLocalizations.unknown
+      : _appLocalizations.abbrWeek(dateTime);
 
   ///12小时制时间国际化
-  String jm([DateTime? dateTime]) =>
-      dateTime == null ? "" : _appLocalizations.jm(dateTime);
+  String jm([DateTime? dateTime]) => dateTime == null
+      ? _appLocalizations.unknown
+      : _appLocalizations.jm(dateTime);
 
   ///时间为当日只显示时间
   String datetimeOrTime([DateTime? dateTime]) {
-    if (dateTime == null) return "";
+    if (dateTime == null) return _appLocalizations.unknown;
     int difference = dateTime.calculateDifference() ?? -1;
     if (difference == 0) {
       return jm(dateTime);
